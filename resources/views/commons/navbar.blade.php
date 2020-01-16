e4371e02fe9ce79aac5d9b72b4a41932f32adcb0
K.H <englishmailmagazine180718@gmail.com>
2020-01-16 12:36:27 +0000

    edit navbar and top

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header class="mb-4">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">TaskList</a>
+            
+            <button type="button" class="navbar-toggler" data-toggler="collapse" data-target="#nav-bar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="nav-bar">
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                    @if(Auth::check())
+                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加',[], ['class'=> 'nav-link']) !!}</li>
+                    @else
+                        <li class="nav-item">{!! link_to_route /*signup*/ !!}</li>
+                        <li class="nav-item">{!! link_to_route /*login*/ !!}</li>
+                    @endif
+                </ul>
+            </div>
+        </nav>
+</header>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h1>タスク一覧</h1>
+    @if(Auth::check()){
+        <h1>タスク一覧</h1>
+        
+        @if (count($tasks)>0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>タスク</th>
+                        <th>ステータス</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id'=>$task->id])!!}</td>
+                        <td>{{$task->content}}</td>
+                        <td>{{$task->status}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
     
-    @if (count($tasks)>0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                    <th>ステータス</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id'=>$task->id])!!}</td>
-                    <td>{{$task->content}}</td>
-                    <td>{{$task->status}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-    
-    {!! link_to_route('tasks.create', '新規タスクの追加',[], ['class'=>'btn btn-primary']) !!}
+        {!! link_to_route('tasks.create', '新規タスクの追加',[], ['class'=>'btn btn-primary']) !!}
+    @else
+       <div class="center jumbotron">
+           <div class="text-center">
+               <h1>TaskList</h1>
+           </div>
+       </div> 
 @endsection

389f938bc995066bb73a154348af6c5090464be2
K.H <englishmailmagazine180718@gmail.com>
2020-01-16 21:28:49 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::resource('tasks', 'TasksController');
 //ユーザ登録機能
 Route::get('signup', 'Auth\RegisterController@showRegistration')->name('signup.get');
 Route::post('signup', 'AUth\RegisterController@register')->name('signup.post');
+
+//ログイン認証
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
